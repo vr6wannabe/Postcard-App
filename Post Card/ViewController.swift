f843f5195811462506f102842e35697a67c955db
kurtis tryber <kurtistryber@kurtiss-MacBook-Pro.local>
2015-03-12 23:04:41 -0400

    first rev of project

--- Post Card/ViewController.swift
+++ Post Card/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMesageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +25,18 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden = false
+        messageLabel.text = enterMesageTextField.text
+        messageLabel.textColor = UIColor.redColor()
+        
+        
+        enterMesageTextField.text = ""
+        enterMesageTextField.resignFirstResponder()
+        
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+    
     }
 
 
